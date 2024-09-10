8f0569f98e534a69372848e7ed27a62b3fea5911
Vaibhav2321 <vaibhavverma2321@gmail.com>
2024-09-10 22:47:08 +0530

    signup page added in app directory

--- src/app/signUp/page.jsx
+++ src/app/signUp/page.jsx
@@ -1,5 +1,7 @@
+"use client"
+
 import { useState } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation'
 
 export default function signUp() {
   const [name, setName] = useState('');
@@ -13,7 +15,7 @@ export default function signUp() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // Ensure the URL matches your API route
+    // Ensure the URL matches your API routejjju
     const res = await fetch('/api/signUp', {
       method: 'POST',
       headers: {
